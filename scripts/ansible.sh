90c50e8d7370e8d9208f04732055dcdf0f919305
Joshua Haupt <joshua@hauptj.com>
2018-05-11 11:20:52 -0500

    fixed syntax errors

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -9,10 +9,13 @@
 
 
 # Enable EPEL REPO
-yum -y --enablerepo=extras install epel-release
+#yum -y --enablerepo=extras install epel-release
+rpm -ivh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+yum -y update
+
 
 # Install requirements for Ansible provisioning
-yum install -y \
+yum -y install \
   ansible \
   git \
   wget
